3bf5a1fbe4099fefe1ce739bf4919ce43bcfe4e4
lingfeng <wb-lwx502637@antfin.com>
2019-02-20 17:20:33 +0800

    Initial commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/ToastUtils.java
@@ -0,0 +1,14 @@
+package com.example.mylibrary;
+
+import android.util.Log;
+
+/**
+ * Created by wb-lwx502637 on 2019/2/20.
+ */
+
+public class ToastUtils {
+
+    public static void logs() {
+        Log.e("ToastUtils", "logs: ToastUtils" );
+    }
+}
